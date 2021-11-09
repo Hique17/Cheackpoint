eb3972d514173222351414908b229ac4bc44f9c8
Henrique Venancio Dos Santos <hqsantos1@gmail.com>
2021-11-09 20:30:04 -0300

    checkpoint de programção imperativa!

---
+++ micro_ondas.js
@@ -0,0 +1,93 @@
+let  pipoca = "10 segundos";
+
+if (pipoca == "9 segundos"){console.log ("tempo insuficiente");}
+
+else if(pipoca == "20 segundos "){console.log("comida queimou");}
+
+
+
+else if(pipoca == "30 segundos "){console.log("kabumm");}
+
+
+else if(pipoca == "10 segundos"){console.log("Prato pronto, bom apetite!!!");}
+
+else{console.log("Prato inexistente");}
+
+
+
+
+
+
+let  Macarrão = "8 segundos";
+
+if (Macarrão == "7 segundos"){console.log ("tempo insuficiente");}
+
+else if(Macarrão == "14 segundos "){console.log("comida queimou");}
+
+
+
+else if(Macarrão == "24 segundos "){console.log("kabumm");}
+
+
+else if(Macarrão == "8 segundos"){console.log("Prato pronto, bom apetite!!!");}
+
+else{console.log("Prato inexistente");}
+
+
+
+
+
+let  carne = "15 segundos";
+
+if (carne == "14 segundos"){console.log ("tempo insuficiente");}
+
+else if(carne == "30 segundos "){console.log("comida queimou");}
+
+
+
+else if(carne == "45 segundos "){console.log("kabumm");}
+
+
+else if(carne == "15 segundos"){console.log("Prato pronto, bom apetite!!!");}
+
+else{console.log("Prato inexistente");}
+
+
+
+
+
+
+let  Feijão = "12 segundos";
+
+if (Feijão == "11 segundos"){console.log ("tempo insuficiente");}
+
+else if(Feijão == "24 segundos"){console.log("comida queimou");}
+
+else if(Feijão == "36 segundos"){console.log("kabumm");}
+
+
+else if(Feijão == "12 segundos"){console.log("Prato pronto, bom apetite!!!");}
+
+else{console.log("Prato inexistente");}
+
+
+
+
+
+let  Brigadeiro = "8 segundos";
+
+if (Brigadeiro == "7 segundos"){console.log ("tempo insuficiente");}
+
+else if(Brigadeiro == "14 segundos "){console.log("comida queimou");}
+
+else if(Brigadeiro == "24 segundos "){console.log("kabumm");}
+
+
+else if(Brigadeiro == "8 segundos"){console.log("Prato pronto, bom apetite!!!");}
+
+else{console.log("Prato inexistente");}
+
+
+
+
+
